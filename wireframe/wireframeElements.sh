8cf53e722f0647819b65f361c221ebe106d8f148
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-03-04 15:29:47 +1100

    Initial

---
+++ wireframe/wireframeElements.sh
@@ -0,0 +1,21 @@
+
+rmdir -rf wireframeImages/
+mkdir -p wireframeImages
+
+
+  ./makeElement.sh style_orientation_orientation "horizontal" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh style_even_layout_weight "1" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh style_large_layout_weight "3" input 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh User_User_Users "" list 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Tab_Group_Tab_ID "" input 1 false false true true false wireframeImages/
+  
+  ./makeElement.sh Tab_Group_Tab_Attach_New_Files "" button 1 false false false false false wireframeImages/
+  
+  ./makeElement.sh Tab_Group_Files_Files "" file 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Tab_Group_Files_Button_Files "" button 1 false false false false false wireframeImages/
+  
