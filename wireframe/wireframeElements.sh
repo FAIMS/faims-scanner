86134946c94431430e9fb122be4bc5c3027ee60e
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-03-04 18:11:15 +1100

    Autosaving doesn't work yet

--- wireframe/wireframeElements.sh
+++ wireframe/wireframeElements.sh
@@ -11,11 +11,33 @@ mkdir -p wireframeImages
   
   ./makeElement.sh User_User_Users "" list 1 false false false false false wireframeImages/
   
-  ./makeElement.sh Tab_Group_Tab_ID "" input 1 false false true true false wireframeImages/
+  ./makeElement.sh Scan_Record_Tab_Do "" button 1 false false false false false wireframeImages/
   
-  ./makeElement.sh Tab_Group_Tab_Attach_New_Files "" button 1 false false false false false wireframeImages/
+  ./makeElement.sh Scan_Record_Tab_ID "" input 1 false false true true false wireframeImages/
   
-  ./makeElement.sh Tab_Group_Files_Files "" file 1 false false true false false wireframeImages/
+  ./makeElement.sh Scan_Record_Tab_Attach_New_Files "" button 1 false false false false false wireframeImages/
   
-  ./makeElement.sh Tab_Group_Files_Button_Files "" button 1 false false false false false wireframeImages/
+  ./makeElement.sh Scan_Record_Tab_File_Type "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Language "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Repository "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Garrison "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Period_From "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Period_Till "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Doc_Name "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_File_Reference "" input 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Theme "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Tab_Sub-Theme "" dropdown 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Files_Files "" file 1 false false true false false wireframeImages/
+  
+  ./makeElement.sh Scan_Record_Files_Button_Files "" button 1 false false false false false wireframeImages/
   
