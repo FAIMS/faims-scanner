3fc8cc7cf4da65388cf747bd1197b39ec489b336
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-06-29 19:23:21 +1000

    Latest changes from client

--- postproc.sh
+++ postproc.sh
@@ -2,72 +2,80 @@
 
 cd module
 
-string="
-newScanRecord(){
-  String tabgroup = \"Scan_Record\";
-
-  String autoNumSource = \"\";
-  autoNumSource = getFieldValue(\"Control\/Control\/Next_ID\");
-  if (isNull(autoNumSource)) {
-    showWarning(\"{Alert}\",\"{A_next_ID_has_not_been_entered_please_provide_one}\");
-    return;
-  }
-
-  setUuid(tabgroup, null);
-  newTabGroup(tabgroup);
-
-  String autoNumDest = \"\";
-  autoNumDest = \"Scan_Record\/Tab\/ID\";
-  incAutoNum(autoNumDest);
-}"
-replacement="
-newScanRecord(){
-  String tabgroup = \"Scan_Record\";
-
-  String autoNumSource = \"\";
-  autoNumSource = getFieldValue(\"Control\/Control\/Next_ID\");
-  if (isNull(autoNumSource)) {
-    showWarning(\"{Alert}\",\"{A_next_ID_has_not_been_entered_please_provide_one}\");
-    return;
-  }
-
-  setUuid(tabgroup, null);
-  newTabGroup(tabgroup);
-
-  String autoNumDest = \"\";
-  autoNumDest = \"Scan_Record\/Tab\/ID\";
-  incAutoNum(autoNumDest);
-
-  initialisePeriods();
-  initialiseSelected();
-}"
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
-
-string="
-loadScanRecordFrom(String uuid) {
-  String tabgroup = \"Scan_Record\";
-  setUuid(tabgroup, uuid);
-  if (isNull(uuid)) return;
-
-  showTabGroup(tabgroup, uuid);
-}"
-replacement="
-loadScanRecordFrom(String uuid) {
-  String tabgroup = \"Scan_Record\";
-  setUuid(tabgroup, uuid);
-  if (isNull(uuid)) return;
-
-  cb = new FetchCallback() {
-    onFetch(result) {
-      entity = result;
-      attributes = entity.getAttributes();
-      initialiseSelected(attributes);
-    }
-  };
-  showTabGroup(tabgroup, uuid, cb);
-}"
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
-
-rm ui_logic.bsh.original
-
 echo "No_measurements_to_take=No measurements to take" >> english.0.properties
+
+echo "
+.button {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.input-field {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.text-area {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.gallery {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.file-gallery {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.file-list {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.dropdown {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.checkbox {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.radio-group {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.list {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.date-picker {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.time-picker {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.web-view {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.table-view {
+  margin-left: 20%;
+  width:       80%;
+}
+
+.required {
+  margin-left: 20%;
+  width:       80%;
+}" >> ui_styling.css
